d9be831bab90285f83b798c09c3c9c544b0bba6c
sohailkhan5033 <ahamadsohail5033@gmail.com>
2025-09-17 16:54:40 +0500

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],   // tells Vite to handle React/JSX
+  plugins: [react()],
+  base: "/Wedding-card/", // 👈 important for GitHub Pages
   server: {
-    host: "0.0.0.0",    // allows access from other devices (like your phone)
-    port: 5173          // sets the dev server port (default is 5173)
+    host: "0.0.0.0",
+    port: 5173
   }
 });
